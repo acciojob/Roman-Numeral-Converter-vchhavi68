c8e4d759370f86640fd891127dd46761310efa5e
vchhavi68 <vchhavi68@gmail.com>
2026-01-06 22:47:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function convertToRoman(num) {
 
   const symbols = [
     ['M', 1000],
+    ['CM', 900],
     ['D', 500],
+    ['CD', 400],
     ['C', 100],
+    ['XC', 90],
     ['L', 50],
+    ['XL', 40],
     ['X', 10],
+    ['IX', 9],
     ['V', 5],
+    ['IV', 4],
     ['I', 1]
   ];
 
@@ -22,6 +28,7 @@ function convertToRoman(num) {
 
   return result;
 }
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 

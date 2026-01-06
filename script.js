9d79326ff4dc6eae9f663f93657becdfc3134f1c
vchhavi68 <vchhavi68@gmail.com>
2026-01-06 22:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function convertToRoman(num) {
+  if (num === 0) return "";
 
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const symbols = [
+    ['M', 1000],
+    ['D', 500],
+    ['C', 100],
+    ['L', 50],
+    ['X', 10],
+    ['V', 5],
+    ['I', 1]
+  ];
 
-  //your code here
-	let result = '';
+  let result = "";
 
-  for (let [symbol, value] of values) {
+  for (let [symbol, value] of symbols) {
     while (num >= value) {
       result += symbol;
       num -= value;
@@ -22,12 +22,10 @@ function convertToRoman(num) {
 
   return result;
 }
-
-
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(convertToRoman(36));
 
+  
 
 
 
